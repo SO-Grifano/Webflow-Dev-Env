1b9179669ae6e69af4a6191db236a84e1cef3495
Serhii Orlenko <sorlenko@grifano.com>
2023-10-21 10:37:46 +0200

    clean project

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
 console.log("Webflow-Dev-Env is connected...");
-
-import Splide from "@splidejs/splide";
-
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide(".splide", { perPage: 2 }).mount();
-});
